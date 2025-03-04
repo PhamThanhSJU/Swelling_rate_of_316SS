ff76ff5afe9d308fc58be6183556d6c51efebff5
PhamThanhSJU <146055328+PhamThanhSJU@users.noreply.github.com>
2025-03-04 10:08:20 +0900

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -33,42 +33,42 @@ st.title ("Hybrid machine learning with optimization algorithm and resampling me
 #Make a prediction
 st.header("Predicting the swelling rate of irradiated 316 stainless steels")
 st.subheader("Input variables")
-col1, col2, col3, col4, col5 =st.columns(5)
+col1, col2, col3, col4, col5, col6 =st.columns(6)
 
 with col1:
    X1=st.slider("Fe (wt. %)", 62.70, 71.21)
    X2 = st.slider("Cr (wt. %)", 14.86, 18.97)
    X3= st.slider("Mn (wt. %)", 0.01, 3.89)
    X4 = st.slider("Si (wt. %)", 0.01, 1.96)
-   X5 = st.slider("Co (wt. %)", 0.00, 4.45)
 	
 with col2:	
-
+   X5 = st.slider("Co (wt. %)", 0.00, 4.45)
    X6= st.slider("Mo (wt. %)", 0.01, 4.93)
    X7 = st.slider("Ni (wt. %)", 9.96, 14.04)
-   X8=st.slider("C (wt. %)", 0.00, 0.13)	
+   X8=st.slider("C (wt. %)", 0.00, 0.13)
+	
+with col3:	
    X9 = st.slider("N (wt. %)", 0.00, 0.13)
-   X10= st.slider("B /1000 (wt. %)", 0.00, 5.70)
-
-with col3:		
+   X10= st.slider("B /1000 (wt. %)", 0.00, 5.70)	
    X11 = st.slider("P /100 (wt. %)", 0.10, 4.00)
    X12 = st.slider("S /100 (wt. %)", 0.40, 1.90)
+	
+with col4:	
    X13= st.slider("Al (wt. %)", 0.00, 0.10)
    X14 = st.slider("Ti /100 (wt. %)", 0.00, 2.00)   
    X15=st.slider("Nb /100 (wt. %)", 0.00, 1.00)	
-	
-with col4:	
    X16 = st.slider("Ta /100 (wt. %)", 0.00, 2.00)
+	
+with col5:	
    X17= st.slider("Pb /100 (wt. %)", 0.00, 2.00)
    X18 = st.slider("Cu (wt. %)", 0.00, 4.47)
-   X19 = st.slider("Pre-irr. flue. x10^22(n/cm2)", 0.00, 3.86)
+   X19 = st.slider("Pre-irr. flue. x10^22(n/cm2)", 0.00, 3.86)	
+   X20= st.slider("Irr. flue. x10^22(n/cm2)", 1.53, 14.00)
 	
-with col5:		
-   X20= st.slider("Irr. flue. x10^22(n/cm2)", 1.53, 14.00)	
+with col6:	
    X21 = st.slider("Temperature (C)", 376.00, 780.10)  
    X22=st.slider("Stress (MPa)", 0.0, 403.0)
    X23 = st.slider("Disl. Dens. x10^14(1/m2)", 1.50, 30.00)
-
 
 
 from sklearn.model_selection import train_test_split, KFold
@@ -89,25 +89,39 @@ X_ori = df[['Fe','Cr','Mn','Si','Co','Mo','Ni','C','N','B','P','S','Al','Ti','Nb
 y = df['Swell'].values
 
 X = scaler.fit_transform(X_ori)	
-X_train=X
-y_train=y
-
-n_estimators = 502
-learning_rate = 0.223
-depth = 4
-l2_leaf_reg = 7
-        
-cat_clf_n = CatBoostRegressor(n_estimators =n_estimators,learning_rate = learning_rate, depth=depth,l2_leaf_reg=l2_leaf_reg, random_state=42)
-cat_clf_n.fit(X_train, y_train)
-
+from numpy import asarray
 
 Inputdata = [X1, X2, X3, X4, X5, X6, X7, X8, X9, X10/1000, X11/100, X12/100, X13, X14/100, X15/100, X16/100, X17/100,X18, X19, X20, X21, X22, X23*100000000000000]
 
+X_predict = asarray([Inputdata])
+predictions = []
+  
+for i in range(1,11,1):
+    count = i
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.10, random_state=(0+i))
 
-from numpy import asarray
-Newdata1 = asarray([Inputdata])
-print(Newdata1)
-Newdata=scaler.transform(Newdata1)
+    n_estimators = 502
+    learning_rate = 0.223
+    depth = 4
+    l2_leaf_reg = 7
+
+    
+    cat_clf_n = CatBoostRegressor(n_estimators=n_estimators,learning_rate = learning_rate, depth=depth,l2_leaf_reg=l2_leaf_reg, random_state=42)
+    cat_clf_n.fit(X_train, y_train)
+
+
+    # X_predict1= scaler.fit_transform(X_predict)
+    X_predict1=scaler.transform(X_predict)
+
+    result_xgb = cat_clf_n.predict(X_predict1)
+
+    predictions.append(result_xgb)
+	
+mean_pred = np.mean(predictions)
+std_pred = np.std(predictions)
+
+puxgb = np.around(mean_pred, 3)
+puxgb_err = np.around(std_pred, 3)
 
 fc_pred2 = cat_clf_n.predict(Newdata)
 
@@ -122,5 +136,5 @@ if st.button("Predict"):
        my_bar.progress(percent_complete + 1, text=progress_text)
     time.sleep(1)
     my_bar.empty()
-    st.success(f"Your predicted swelling rate (%) obtained from WERCS-FHO-CGB model is: {(fc_pred2)}")
-
+    st.success(f"Your predicted swelling rate (%) obtained from WERCS-FHO-CGB model is: {(puxgb)}")
+    st.success(f"The standard deviation (Uncertainty) (%) obtained from proposed model is: {(puxgb)}")
